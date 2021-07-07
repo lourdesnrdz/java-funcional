4a228336c0b68c46fbcd74f94c39dbbc72f19241
lourdesnrdz <lourdesnrdz@gmail.com>
2021-07-07 11:45:22 -0500

    Understanding Streams

---
+++ job-search/modules/src/main/java/com/platzi/functional/_15_streams_intro/Streams.java
@@ -0,0 +1,45 @@
+package com.platzi.functional._15_streams_intro;
+
+import com.platzi.functional._06_reference_operator.NombresUtils;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Streams {
+    public static void main(String[] args) {
+        List<String> courseList = NombresUtils.getList(
+                "Java", 
+                "FrontEnd", 
+                "BackEnd", 
+                "FullStack");
+        for (String course :
+                courseList) {
+            String newCourseName = course.toLowerCase().replace("!", "!!");
+            System.out.println("Platzi: " + newCourseName);
+        }
+
+        Stream<String> coursesStream = Stream.of("Java",
+                "FrontEnd",
+                "BackEnd",
+                "FullStack");
+
+//        Stream<Integer> courseLengthStream = coursesStream.map(course -> course.length());
+
+//        Optional<Integer> longest = courseLengthStream.max((x, y) -> y - x);
+
+        Stream<String> emphasisCourses = coursesStream.map(course -> course + "!");
+
+        Stream<String> justJavaCourses = emphasisCourses.filter(course -> course.contains("Java"));
+
+        justJavaCourses.forEach(System.out::println);
+
+
+
+        
+    }
+
+
+
+}
